8092ac74142711b04296b0ca97e024e6cec3002d
StrixzIV <46487993+JirayuKaewsing@users.noreply.github.com>
2021-07-12 09:49:30 +0700

    added comments
    main code for face detection

--- opencvtest.py
+++ opencvtest.py
@@ -1,20 +1,31 @@
 import cv2
 
+#set input as webcam 
 cap = cv2.VideoCapture(0)
-face_cascade = faceCascade = cv2.CascadeClassifier('Machine Learning/OpenCV/OpenCV Face recognition/haarcascade_frontalface_default.xml')
 
+#select haarcascade datasets
+face_cascade = faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+#set output resolutions
 cap.set(3, 1280)
 cap.set(4, 720)
 
 while cap.isOpened():
+    
+    #read the input image
     _, img = cap.read()
+    
+    #change image to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    #detect the face
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
+    #Draw rectangle on face
     for (x, y, w, h) in faces:
             cv2.rectangle(img, (x, y), (x+w, y+h), (255, 255, 0), 3)
 
-    # Display the Image
+    #Exit key
     cv2.imshow('Image', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
